11a99e1d065e173a8381eb5bce0f7457afb177d4
zhujq2018 <78999765+zhujq2018@users.noreply.github.com>
2022-05-03 00:44:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,11 @@ chmod 600 /root/.ssh/authorized_keys
 echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
 mkdir -p /var/run/sshd
 nohup /usr/sbin/sshd -D &
+mkdir -p /frp
+cd /frp
+wget https://github.com/fatedier/frp/releases/download/v0.42.0/frp_0.42.0_linux_amd64.tar.gz
+tar -zxvf frp_0.42.0_linux_amd64.tar.gz
+cd frp_0.42.0_linux_amd64
 
 chmod +x ./getpubip
  ./getpubip
