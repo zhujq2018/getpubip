38e425df8ef9c5da3a3e0dd007158b02b70742c1
zhujq2018 <78999765+zhujq2018@users.noreply.github.com>
2022-05-03 00:40:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+export USER=root
+mv /authorized_keys /root/.ssh/authorized_keys
+mv /id_rsa /root/.ssh/id_rsa
+mv /id_rsa.pub /root/.ssh/id_rsa.pub
+chmod 600 /root/.ssh/id_rsa
+chmod 644 /root/.ssh/id_rsa.pub
+chmod 600 /root/.ssh/authorized_keys
+echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
+mkdir -p /var/run/sshd
+nohup /usr/sbin/sshd -D &
+
 chmod +x ./getpubip
  ./getpubip
