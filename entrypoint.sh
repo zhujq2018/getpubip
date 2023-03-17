192bcff2b379758e9656c1350ad97cac0db2ea2f
zhujq2018 <78999765+zhujq2018@users.noreply.github.com>
2023-03-17 23:13:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\
 
 mkdir -p /root/tail
 cd /root/tail
-wget https://pkgs.tailscale.com/stable/${TSFILE}
-tar xzf {TSFILE} --strip-components=1
+wget -O ts.tgz https://pkgs.tailscale.com/stable/${TSFILE}
+tar xzf ts.tgz --strip-components=1
 mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
 nohup ./tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 ./tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=render-vps
