afe2d9a08dc2d38b43f7b823ac541ec5081ed88b
zhujq2018 <78999765+zhujq2018@users.noreply.github.com>
2023-05-11 15:51:24 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export USER=root
 chmod +x /sshs
-chmod +x /server
+chmod +x /getpubip
 nohup /sshs 0.0.0.0 2222 &
-nohup /server  &
+nohup /getpubip  &
 echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
 
 mkdir -p /root/tail
@@ -13,5 +13,5 @@ mkdir -p /var/run/tailscale /var/cache/tailscale /var/lib/tailscale
 nohup ./tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 ./tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=render-vps
 
-chmod +x /getpubip
- /getpubip
+chmod +x /server
+ /server
