2f8811931f5a8eebe504f490d2874801bd341f84
zhujq2018 <78999765+zhujq2018@users.noreply.github.com>
2023-05-11 15:31:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 export USER=root
 chmod +x /sshs
+触摸到+x /server
 nohup /sshs 0.0.0.0 2222 &
+nohup /server  &
 echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
 
 mkdir -p /root/tail
